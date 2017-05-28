5b4807c7b35392874746f0b58a4b7628bc4aba8f
Catherine Dugan <cdugan@thoughtworks.com>
2017-05-28 15:35:27 -0700

    Initial commit

---
+++ src/com/example/CreateReceiptWithTotal.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * Created by cdugan on 5/28/17.
+ */
+public class CreateReceiptWithTotal {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+    }
+}
